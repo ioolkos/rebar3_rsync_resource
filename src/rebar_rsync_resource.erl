53eb6aa50db8e0de7cf5833a48e48c205f5c8d56
Dmitry Ivanov <divanov@machinezone.com>
2015-09-16 17:23:08 +0600

    Use rsync --dry-run to decide if update is needed

--- src/rebar_rsync_resource.erl
+++ src/rebar_rsync_resource.erl
@@ -9,16 +9,22 @@
     , make_vsn/1
     ]).
 
+flags() -> "--exclude _build -aW".
+
 lock(Dir, Source) ->
     Source.
 
 download(Dir, {rsync, Remote}, State) ->
-    Out = os:cmd(io_lib:format("rsync --exclude _build -aW ~s ~s", [Remote, Dir])),
+    Out = os:cmd(io_lib:format("rsync ~s ~s ~s", [flags(), Remote, Dir])),
     io:format(Out),
     {ok, Out}.
 
-needs_update(Dir, Source) ->
-    false.
+needs_update(Dir, {rsync, Remote}) ->
+    Out = os:cmd(io_lib:format("rsync --stats --dry-run ~s ~s ~s", [flags(), Remote, Dir])),
+    case string:str(Out, "Total transferred file size: 0 bytes") of
+        0 -> true;
+        _ -> false
+    end.
 
 make_vsn(Dir) ->
     {plain, "42"}.
